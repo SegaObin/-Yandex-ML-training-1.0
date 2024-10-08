ff0977f9fea52c320e41e0fc07bfda99a8dda831
Григорьев Сергей <chehovAnnt@yandex.ru>
2024-10-08 23:41:59 +0300

    Add files via upload

---
+++ Laplace distribution/distribution.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+class LaplaceDistribution:    
+    @staticmethod
+    def mean_abs_deviation_from_median(x: np.ndarray):
+        '''
+        Args:
+        - x: A numpy array of shape (n_objects, n_features) containing the data
+          consisting of num_train samples each of dimension D.
+        '''
+        ####
+        # Do not change the class outside of this block
+        return np.std(x)
+        ####
+
+    def __init__(self, features):
+        '''
+        Args:
+            feature: A numpy array of shape (n_objects, n_features). Every column represents all available values for the selected feature.
+        '''
+        ####
+        # Do not change the class outside of this block
+        self.loc = np.median(features, axis=0)
+        self.scale = np.mean(np.abs(features - self.loc), axis=0)
+        ####
+
+
+    def logpdf(self, values):
+        '''
+        Returns logarithm of probability density at every input value.
+        Args:
+            values: A numpy array of shape (n_objects, n_features). Every column represents all available values for the selected feature.
+        '''
+        ####
+        # Do not change the class outside of this block
+       
+        no_null_scale = np.where(self.scale == 0, 1e-10, self.scale)
+        # Calculate the absolute difference from the location
+        abs_diff = np.abs(values - self.loc)
+
+        # Compute the log PDF
+        log_density = -np.log(2 * no_null_scale) - (abs_diff / no_null_scale)
+
+        return log_density
+        ####
+        
+    
+    def pdf(self, values):
+        '''
+        Returns probability density at every input value.
+        Args:
+            values: A numpy array of shape (n_objects, n_features). Every column represents all available values for the selected feature.
+        '''
+        return np.exp(self.logpdf(value))
